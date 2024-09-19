38d8ec084530e2c8581a5b6b225223ace5ebb01d
CorentinDWWM <newsonic62@gmail.com>
2024-09-19 16:01:53 +0200

    push

--- src/images.js
+++ src/images.js
@@ -1,4 +1,12 @@
 import ecd from "./assets/img/espacecoiffuredeux.jpg";
+import es from "./assets/img/espaceshampooing.jpg";
+import ed from "./assets/img/espacedetente.jpg";
+import ga from "./assets/img/gammeastera.jpg";
+import gcg from "./assets/img/gammecolorglow.jpg";
+import gsk from "./assets/img/gammesublimek.jpg";
+import go from "./assets/img/gammeokara.jpg";
+import gc from "./assets/img/gammecurbicia.jpg";
+import gt from "./assets/img/gammetriphasic.jpg";
 
 export const images1 = [
   {
@@ -6,48 +14,48 @@ export const images1 = [
     texte: "Espace Coiffure",
   },
   {
-    lien: "src/assets/img/espaceshampooing.jpg",
+    lien: es,
     texte: "Espace Shampooing",
   },
   {
-    lien: "src/assets/img/espacedetente.jpg",
+    lien: ed,
     texte: "Espace Détente",
   },
 ];
 
 export const images2 = [
   {
-    lien: "src/assets/img/gammeastera.jpg",
+    lien: ga,
     texte: "Gamme Astera",
     sous_texte:
       "Le shampoing pour cuir chevelu sensible qui nettoie en douceur et protège, avec une formule haute tolérance aux actifs 100% d’origine naturelle.",
   },
   {
-    lien: "src/assets/img/gammecolorglow.jpg",
+    lien: gcg,
     texte: "Gamme ColorGlow",
     sous_texte:
       "Le shampooing soin professionnel sans sulfate* qui nettoie en douceur les cheveux colorés ou méchés et prévient l'affadissement de la couleur jusqu'à 10 semaines.",
   },
   {
-    lien: "src/assets/img/gammesublimek.jpg",
+    lien: gsk,
     texte: "Gamme Sublime Karité",
     sous_texte:
       "Le shampooing à l'action 2 en 1 qui hydrate durablement et discipline des racines aux pointes.",
   },
   {
-    lien: "src/assets/img/gammeokara.jpg",
+    lien: go,
     texte: "Gamme Okara",
     sous_texte:
       "Le shampoing qui ravive la luminosité des cheveux blonds naturels, méchés ou décolorés.",
   },
   {
-    lien: "src/assets/img/gammecurbicia.jpg",
+    lien: gc,
     texte: "Gamme Curbicia",
     sous_texte:
       "Le shampooing anti cheveux gras aux actifs 100% naturels qui purifie et nettoie en douceur les cuirs chevelus à tendance grasse.",
   },
   {
-    lien: "src/assets/img/gammetriphasic.jpg",
+    lien: gt,
     texte: "Gamme Triphasic",
     sous_texte:
       "Le shampoing traitant qui réduit la chute de cheveux dès 4 semaines sans agresser le cuir chevelu et rend la chevelure plus résistante, douce et brillante.",
