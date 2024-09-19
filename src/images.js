8d74824ff88cd57886ea2015df9a6696d3c3ade3
CorentinDWWM <newsonic62@gmail.com>
2024-09-19 15:56:41 +0200

    push

--- src/images.js
+++ src/images.js
@@ -1,6 +1,8 @@
+import ecd from "./assets/img/espacecoiffuredeux.jpg";
+
 export const images1 = [
   {
-    lien: "src/assets/img/espacecoiffuredeux.jpg",
+    lien: ecd,
     texte: "Espace Coiffure",
   },
   {
@@ -15,37 +17,37 @@ export const images1 = [
 
 export const images2 = [
   {
-    lien: "../assets/img/gammeastera.jpg",
+    lien: "src/assets/img/gammeastera.jpg",
     texte: "Gamme Astera",
     sous_texte:
       "Le shampoing pour cuir chevelu sensible qui nettoie en douceur et protège, avec une formule haute tolérance aux actifs 100% d’origine naturelle.",
   },
   {
-    lien: "../assets/img/gammecolorglow.jpg",
+    lien: "src/assets/img/gammecolorglow.jpg",
     texte: "Gamme ColorGlow",
     sous_texte:
       "Le shampooing soin professionnel sans sulfate* qui nettoie en douceur les cheveux colorés ou méchés et prévient l'affadissement de la couleur jusqu'à 10 semaines.",
   },
   {
-    lien: "../assets/img/gammesublimek.jpg",
+    lien: "src/assets/img/gammesublimek.jpg",
     texte: "Gamme Sublime Karité",
     sous_texte:
       "Le shampooing à l'action 2 en 1 qui hydrate durablement et discipline des racines aux pointes.",
   },
   {
-    lien: "../assets/img/gammeokara.jpg",
+    lien: "src/assets/img/gammeokara.jpg",
     texte: "Gamme Okara",
     sous_texte:
       "Le shampoing qui ravive la luminosité des cheveux blonds naturels, méchés ou décolorés.",
   },
   {
-    lien: "../assets/img/gammecurbicia.jpg",
+    lien: "src/assets/img/gammecurbicia.jpg",
     texte: "Gamme Curbicia",
     sous_texte:
       "Le shampooing anti cheveux gras aux actifs 100% naturels qui purifie et nettoie en douceur les cuirs chevelus à tendance grasse.",
   },
   {
-    lien: "../assets/img/gammetriphasic.jpg",
+    lien: "src/assets/img/gammetriphasic.jpg",
     texte: "Gamme Triphasic",
     sous_texte:
       "Le shampoing traitant qui réduit la chute de cheveux dès 4 semaines sans agresser le cuir chevelu et rend la chevelure plus résistante, douce et brillante.",
